dce57fa73d56022fdb031a7b50023412f93baf19
Jillian <jillian.e.rowe@gmail.com>
2021-09-27 15:44:54 +0300

    commit

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,10 +1,12 @@
 import unittest
 from unittest.mock import MagicMock, patch
 import os
-# import jupyter_cellxgene_gateway_proxy
+import jupyter_cellxgene_gateway_proxy
 
 class TestImports(unittest.TestCase):
     def test_imports(self):
-        # my_env = jupyter_cellxgene_gateway_proxy.setup_jupyter_cellxgene_gateway_proxy()['environment']('5001')
-        # assert my_env['PROXY_FIX_HOST'] == 1
-        assert 1 == 1
+        my_env = jupyter_cellxgene_gateway_proxy.setup_jupyter_cellxgene_gateway_proxy()['environment']('5001')
+        assert my_env['PROXY_FIX_HOST'] == 1
+
+if __name__ == '__main__':
+    unittest.main()
